b0865a5dfc83246335158021b89926c9b9876812
NBaille <36842872+MSGOD@users.noreply.github.com>
2023-02-07 15:17:05 -0500

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+
+function Home() {
+  return (
+    <div>
+      <h2>Welcome</h2>
+      <button><Link to="/about">About</Link></button>
+      <br />
+      <button><Link to="/rate">Rate</Link></button>
+    </div>
+  );
+}
+
+function About() {
+  return (
+    <div>
+      <h2>About</h2>
+    </div>
+  );
+}
+
+function Rate() {
+  const [grade, setGrade] = useState(0);
+  const [name, setName] = useState("");
+  const [reasoning, setReasoning] = useState("");
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(grade, name, reasoning);
+  };
+
+  return (
+    <div>
+      <h2>Rate</h2>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Grade (0-10):
+          <input
+            type="number"
+            min="0"
+            max="10"
+            value={grade}
+            onChange={(event) => setGrade(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Name:
+          <input
+            type="text"
+            maxLength="100"
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+        </label>
+        <br />
+        <label>
+          Reasoning:
+          <textarea
+            maxLength="1000"
+            value={reasoning}
+            onChange={(event) => setReasoning(event.target.value)}
+          />
+        </label>
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Route path="/" exact component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/rate" component={Rate} />
+    </Router>
   );
 }
 
